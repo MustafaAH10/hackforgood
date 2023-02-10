9366dd2a11f297f665a4cab1248b62d4446055ed
AravinthAdarsh <111167525+AravinthAdarsh@users.noreply.github.com>
2023-02-10 16:19:12 +0800

    Update patient-sign-up.php

--- patient-sign-up.php
+++ patient-sign-up.php
@@ -1,41 +1,43 @@
 <?php
+    if(isset($_POST['submit']))
+    {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $dob = $_POST['dob'];
+        $disabilities = $_POST['disabilities'];
+        $description = $_POST['description'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $pwd = $_POST['pwd'];
+        
 
-$servername = "";
-$username = "";
-$password = "";
-$dbname = "";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+    }
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "patient";
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    
+    $con =mysqli_connect($host, $username, $pwd, $dbname, 3307);
 
-// Get form data
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
-$dob = $_POST["dob"];
-$gender = $_POST["gender"];
-$selected_languages = implode(", ", $_POST["selected_languages"]);
-$specialities = implode(", ", $_POST["selected_options"]);
-$experience = $_POST["experience"];
-$certificate = $_POST["Certificate"];
-$description = $_POST["description"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+    
+    if (!$con)
+    {
+        die("Connection failed!" . mysqli_connect_error());
+    }
 
-$sql = "INSERT INTO practitioners (first_name, last_name, dob, gender, languages, specialities, experience, certificate, description, phone, email, password) 
-VALUES ('$first_name', '$last_name', '$dob', '$gender', '$selected_languages', '$specialities', '$experience', '$certificate', '$description', '$phone', '$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+    
+    $sql = "INSERT INTO patient_table (first_name, last_name, dob, disabilities, description, phone, email, pwd) VALUES ('$first_name', '$last_name', '$dob', '$disabilities', '$description', '$phone', '$email', '$pwd')";
+  
+    
+    $rs = mysqli_query($con, $sql);
+    if($rs)
+    {
+        echo "Entries added!";
+    }
+  
+    
+    mysqli_close($con);
 
 ?>
